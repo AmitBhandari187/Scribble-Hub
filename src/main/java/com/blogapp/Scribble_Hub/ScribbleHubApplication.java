8e4ee9fdbd9d0fa73e9de6992679c6fdb5f0d6be
AmitBhandari187 <bhandari18ricky@gmail.com>
2026-06-01 22:10:28 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/blogapp/Scribble_Hub/ScribbleHubApplication.java
@@ -0,0 +1,13 @@
+package com.blogapp.Scribble_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribbleHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribbleHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogapp/Scribble_Hub/ScribbleHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogapp.Scribble_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribbleHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
